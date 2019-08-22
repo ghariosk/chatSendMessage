d7ddc063fcf0d4866a99ba4ed8776d07b8c616f5
ghariosk <karlgharios@hotmail.com>
2019-08-22 20:35:14 +0300

    another test

--- chatSendMessage/lambda.js
+++ chatSendMessage/lambda.js
@@ -8,61 +8,63 @@ exports.handler = async function (event, context, callback) {
     // You can pass the existing connection to this function.
     // A new connection will be created if it's not present as the third param 
     // You must always end/destroy the DB connection after it's used
+    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+    });
 
-        var connection= await rds.query({
+    
+
+     function executeQuery (callback) {
+
+      rds.query({
         instanceIdentifier: 'main',
         query: 'SELECT connectionId from chat',
         inserts: [0]
-    }, function (error, results, connection) {
+    }, async function (error, results, connection) {
         if (error) {
             console.log("Error occurred");
             throw error;
         } else {
             console.log("Success")
-            console.log(results);
-            
+        
+            return callback(null, results)
 
             
         }
 
-        connection.end();
-
-        console.log(results)
-        return results
-    });
-    var connectionData = await connection.json()
-
+    connection.end()
+    }
+    )
+}
+    const connectionData = await executeQuery()
     console.log(connectionData)
-    
 
-    
 
-//     const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-//     apiVersion: '2018-11-29',
-//     endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
-//     });
-
-//     const postData = JSON.parse(event.body).data;
+  const postData = JSON.parse(event.body).data;
   
-//   const postCalls = connectionData.map(async ({ connectionId }) => {
-//     try {
-//       await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
-//     } catch (e) {
-//       if (e.statusCode === 410) {
-//         console.log(`Found stale connection, deleting ${connectionId}`);
-//         await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
-//       } else {
-//         throw e;
-//       }
-//     }
-//   });
+  const postCalls =  connectionData.map(async ({ connectionId }) => {
+    try {
+      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+      console.log('done')
+    } catch (e) {
+      if (e.statusCode == 410) {
+        console.log(`Found stale connection, deleting ${connectionId}`);
+      } else {
+          console.log("ERROR")
+        throw e;
+      }
+    }
+  });
   
-//   try {
-//     await Promise.all(postCalls);
-//   } catch (e) {
-//     return { statusCode: 500, body: e.stack };
-//   }
+  try {
+    await Promise.all(postCalls);
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
 
   return { statusCode: 200, body: 'Data sent.' };
+   
+  
  
 }
